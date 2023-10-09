5e1849897bd12ef21feb03883d96218ea4509534
pebruwantoro <pebruwantoro.doni@gmail.com>
2023-10-09 21:38:09 +0700

    fixing

--- internal/app/handler/rest/objective/handler_update.go
+++ internal/app/handler/rest/objective/handler_update.go
@@ -1,6 +1,8 @@
 package objective
 
 import (
+	"strconv"
+
 	"github.com/labstack/echo/v4"
 	"github.com/pebruwantoro/hackathon-efishery/internal/app/usecase/objective"
 	"github.com/pebruwantoro/hackathon-efishery/internal/pkg/response"
@@ -10,7 +12,12 @@ import (
 func (h *handler) Update(c echo.Context) (err error) {
 	ctx := c.Request().Context()
 
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return
+	}
 	req := objective.UpdateObjectiveRequest{
+		Id:        id,
 		UpdatedBy: c.Request().Header.Get("User-Email"),
 	}
 	if err = validator.Validate(c, &req); err != nil {
--- internal/app/usecase/objective/case_update.go
+++ internal/app/usecase/objective/case_update.go
@@ -2,21 +2,27 @@ package objective
 
 import (
 	"context"
+	"errors"
 
 	"github.com/pebruwantoro/hackathon-efishery/internal/app/entity"
 )
 
 func (u *usecase) Update(ctx context.Context, req UpdateObjectiveRequest) error {
-	// MAPPING OBJECT
-	objective := entity.Objective{
-		Name:        req.Name,
-		Description: req.Description,
-		Weight:      req.Weight,
+	dataObjective, err := u.objectiveRepository.GetObjectiveByID(ctx, []int{req.Id})
+	objective := entity.Objective{}
+	if len(dataObjective) > 0 {
+		objective = dataObjective[0]
+	} else {
+		return errors.New("objective not found")
 	}
+	// MAPPING OBJECT
+	objective.Name = req.Name
+	objective.Description = req.Description
+	objective.Weight = req.Weight
 	objective.SetUpdated(req.UpdatedBy)
 
 	// UPDATE OBJECTIVE
-	err := u.objectiveRepository.Update(ctx, &objective)
+	err = u.objectiveRepository.Update(ctx, &objective)
 	if err != nil {
 		return err
 	}
